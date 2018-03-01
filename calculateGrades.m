f96d1c22b5ed0e973d8f0b345625ae1cfd4f2f93
Jared Davis <jared@haiq.us>
2018-02-28 19:02:11 -0600

    checkpoint - complete manipulating variables

--- calculateGrades.m
+++ calculateGrades.m
@@ -4,3 +4,8 @@ assignmentMeans = nanmean(gradesOnly);
 meanMatrix = ones(1,15)' * assignmentMeans;
 curvedGrads = 3.5 * (gradesOnly ./ meanMatrix);
 check = nanmean(curvedGrads);
+curvedGrads(curvedGrads>5) = 5;
+totalGrade = ceil(nanmean(curvedGrads,2));
+letters = 'FDCBA';
+letterGrades = letters(totalGrade);
+disp(letterGrades)
